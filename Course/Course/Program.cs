c23cc32f30ccbafd5e0c9b3275b2981065409256
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-23 18:50:52 -0300

    added operadores aritmeticos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,37 +7,42 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            byte n1 = 126;
-            int n2 = 1000;
-            // for bigger number than this you need long type
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
-            bool completo = false;
-            // char type needs a simple '' to recognize the character
-            char sexo = 'F';
-            char letra = '\u0041'; // unicode para a letra.
-            // float needs a letter f at the end of the float number
-            float n5 = 4.5f;
-            // double
-            double n6 = 4.5;
-            // string is imutable
-            string nome = "Maria Gimenes";
-            // object generico
-            object obj1 = "Patrick O'Connel";
-            object obj2 = 4.3f;
+            //byte n1 = 126;
+            //int n2 = 1000;
 
-            Console.WriteLine(completo);
-            Console.WriteLine(sexo);
-            Console.WriteLine(letra);
-            Console.WriteLine(nome);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
+            //// for bigger number than this you need long type
+            //int n3 = 2147483647;
+            //long n4 = 2147483648L;
+            //bool completo = false;
+
+            //// char type needs a simple '' to recognize the character
+            //char sexo = 'F';
+            //char letra = '\u0041'; // unicode para a letra.
+
+            //// float needs a letter f at the end of the float number
+            //float n5 = 4.5f;
+
+            //// double
+            //double n6 = 4.5;
+
+            //// string is imutable
+            //string nome = "Maria Gimenes";
+            // object generico
+            //object obj1 = "Patrick O'Connel";
+            //object obj2 = 4.3f;
+
+            //Console.WriteLine(completo);
+            //Console.WriteLine(sexo);
+            //Console.WriteLine(letra);
+            //Console.WriteLine(nome);
+            //Console.WriteLine(obj1);
+            //Console.WriteLine(obj2);
+            //Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+            //Console.WriteLine(n3);
+            //Console.WriteLine(n4);
+            //Console.WriteLine(n5);
+            //Console.WriteLine(n6);
             Console.WriteLine("=============================");
 
             int idade = 32;
@@ -51,6 +56,52 @@ namespace Course
 
             // concatenation
             Console.WriteLine(nome2 + " tem " + idade + " e " + saldo.ToString("F2") + " de reais.");
+
+            Console.WriteLine("=============================");
+
+            // conversao implicita e casting
+
+            //double a;
+            //float b;
+            //int c;
+
+            //a = 5.1;
+
+            // When turning a double to a float there is a lost of 4 bit.
+            // Since double is 8 bits whereas float is four.
+            // therefore there is will be a lost of bits.
+            //b = (float)a;
+
+            //================
+
+            //c = (int)a;
+            
+
+            //Console.WriteLine(c);
+
+            Console.WriteLine("=============================");
+
+            // Operadores aritimeticos
+
+            //int a = 4 + 3 * 2;//10
+            //int b = (4 + 3) * 2;//14
+            //int c = 17 % 3; // 2
+
+            // Need use casting (float) or (double) or use a float numer as 10.0
+            //double d = (double) 10 / 8; //1.25
+
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            // Formula de bascara // Math.Pow(x, y) is the "at the power"
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            // square root or raiz quadrada Math.sqrt(x)
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
         }
     }
 }
