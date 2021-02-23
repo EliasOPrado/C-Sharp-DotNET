057bf77a6a61091ea6a9d75e0aee3dc55f358214
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-23 19:33:32 -0300

    added data input

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -43,21 +43,21 @@ namespace Course
             //Console.WriteLine(n4);
             //Console.WriteLine(n5);
             //Console.WriteLine(n6);
-            Console.WriteLine("=============================");
+            //Console.WriteLine("=============================");
 
-            int idade = 32;
-            double saldo = 10.35456;
-            string nome2 = "Maria";
-            // place holder, F2 is the number of numbers to show of a float.
-            Console.WriteLine("{0} tem {1} anos e {2:F2} de reais em saldo.", nome2, idade, saldo);
+            //int idade = 32;
+            //double saldo = 10.35456;
+            //string nome2 = "Maria";
+            //// place holder, F2 is the number of numbers to show of a float.
+            //Console.WriteLine("{0} tem {1} anos e {2:F2} de reais em saldo.", nome2, idade, saldo);
 
-            // interpolacao
-            Console.WriteLine($"{nome2} tem {idade} e {saldo:F2} reais.");
+            //// interpolacao
+            //Console.WriteLine($"{nome2} tem {idade} e {saldo:F2} reais.");
 
-            // concatenation
-            Console.WriteLine(nome2 + " tem " + idade + " e " + saldo.ToString("F2") + " de reais.");
+            //// concatenation
+            //Console.WriteLine(nome2 + " tem " + idade + " e " + saldo.ToString("F2") + " de reais.");
 
-            Console.WriteLine("=============================");
+            //Console.WriteLine("=============================");
 
             // conversao implicita e casting
 
@@ -81,6 +81,8 @@ namespace Course
 
             Console.WriteLine("=============================");
 
+            //===========================
+
             // Operadores aritimeticos
 
             //int a = 4 + 3 * 2;//10
@@ -90,18 +92,39 @@ namespace Course
             // Need use casting (float) or (double) or use a float numer as 10.0
             //double d = (double) 10 / 8; //1.25
 
-            double a = 1.0, b = -3.0, c = -4.0;
+            //double a = 1.0, b = -3.0, c = -4.0;
 
-            // Formula de bascara // Math.Pow(x, y) is the "at the power"
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
-            // square root or raiz quadrada Math.sqrt(x)
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            //// Formula de bascara // Math.Pow(x, y) is the "at the power"
+            //double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            //// square root or raiz quadrada Math.sqrt(x)
+            //double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            //double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
 
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            //Console.WriteLine(delta);
+            //Console.WriteLine(x1);
+            //Console.WriteLine(x2);
 
+            // Entrada de dados ============
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            Console.WriteLine("Voce digitou: " + frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
         }
     }
 }
