b5cbfaa9bffb61d606cc2f35f7302f9627b119ae
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 11:57:40 -0300

    updated triangulo

--- poo/triangulo/triangulo/Program.cs
+++ poo/triangulo/triangulo/Program.cs
@@ -54,11 +54,11 @@ namespace triangulo
             y.B = double.Parse(Console.ReadLine());
             y.C = double.Parse(Console.ReadLine());
 
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            double areaX = x.Area();
+
+
+            double areaY = y.Area();
 
             Console.WriteLine("Area x = " + areaX.ToString("F4"));
             Console.WriteLine("Area Y = " + areaY.ToString("F4"));
--- poo/triangulo/triangulo/Triangulo.cs
+++ poo/triangulo/triangulo/Triangulo.cs
@@ -6,5 +6,12 @@ namespace triangulo
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
     }
 }
