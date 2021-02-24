820defb7c0d6315c31401a66eca8fee2b2bd8970
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 08:56:27 -0300

    addde conditionals

---
+++ exercicio-2/exercicio-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // check if the number is negative
+            Console.Write("add a number:");
+            // add the value
+            int number = int.Parse(Console.ReadLine());
+            Check(number);
+            
+        }
+      static  int Check(int a) { 
+
+            if(a < 0){
+                Console.WriteLine("NEGATIVE");
+            }
+            else
+            {
+                Console.WriteLine("POSITIVE");
+            }
+            return a;
+        }
+    }
+}
