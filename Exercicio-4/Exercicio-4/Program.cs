366a060e1acd954d3ba71c5dd8e54d1d748340ea
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 10:05:01 -0300

    added for loops

---
+++ Exercicio-4/Exercicio-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop
+
+            Console.Write("Add an int value to check if it is within a range:");
+            int input = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < input; i++)
+            {
+                if (Enumerable.Range(10, 20).Contains(i))
+                {
+                    Console.WriteLine(i + "in");
+                }
+                else
+                {
+                    Console.WriteLine(i + "out");
+                }
+            }
+        }
+    }
+}
