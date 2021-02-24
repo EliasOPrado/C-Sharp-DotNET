c66b9c107a0353f7f51a0938e4643dbe190a0408
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 14:40:44 -0300

    added contrutores

---
+++ Construtores/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Construtores
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmStoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoveProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome +
+                ", $" +
+                ", " +
+                Preco +
+                ", $" +
+                Preco +
+                ", " +
+                Quantidade +
+                " unidades, total: " +
+                ValorTotalEmStoque();
+        }
+    }
+}
