e01ab3ef86496e04d9c12831076dd294db34fdac
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 14:00:52 -0300

    exercicio 3

---
+++ poo/poo-exercicio-3/poo-exercicio-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace poo_exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.WriteLine("Adicione o nome do funcionario:");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+
+            Console.Write("Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionario: " + f.Nome + ", " + "salario $" + f.SalarioLiquido());
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para almentar o salario: ");
+            double salario = double.Parse(Console.ReadLine());
+            f.AumentarSalario(salario);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + f);
+            
+
+        }
+    }
+}
