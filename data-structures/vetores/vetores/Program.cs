e5cf78a3cbd88435e1c8fe194ff0505f57a93ccd
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 21:39:23 -0300

    vetores adicionados

---
+++ data-structures/vetores/vetores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace vetores {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++) {
+                vect[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine(vect);
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("Altura media: " + avg);
+        }
+    }
+}
--- poo/Construtores/construtores/Produto.cs
+++ poo/Construtores/construtores/Produto.cs
@@ -1,87 +1,87 @@
-﻿using System;
-namespace construtores
-{
-    public class Produto
+﻿    using System;
+    namespace construtores
     {
-
-        private string _nome;
-        private double _preco;
-        private int _quantidade;
-
-        public Produto(string nome, double preco, int quantidade)
+        public class Produto
         {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
-        }
 
-        public Produto(string nome, double preco)
-        {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = 5;
-        }
+            private string _nome;
+            private double _preco;
+            private int _quantidade;
 
-        // you need to use set and get to get private instances of an constructor
-        public string GetNome()
-        {
-            return _nome;
-        }
-
-        public void SetNome(string nome)
-        {
-            // this encapsulization is used for security reasons and set filters.
-            if(nome != null && nome.Length > 1) {
+            public Produto(string nome, double preco, int quantidade)
+            {
                 _nome = nome;
+                _preco = preco;
+                _quantidade = quantidade;
             }
 
-        }
+            public Produto(string nome, double preco)
+            {
+                _nome = nome;
+                _preco = preco;
+                _quantidade = 5;
+            }
 
-        public string Nome
-        {
-            get { return _nome; }
-            set {
-                // to use parameter you need to change nome by value
-                if (value != null && value.Length > 1)
-                {
-                    _nome = value;
+            // you need to use set and get to get private instances of an constructor
+            public string GetNome()
+            {
+                return _nome;
+            }
+
+            public void SetNome(string nome)
+            {
+                // this encapsulization is used for security reasons and set filters.
+                if(nome != null && nome.Length > 1) {
+                    _nome = nome;
+                }
+
+            }
+
+            public string Nome
+            {
+                get { return _nome; }
+                set {
+                    // to use parameter you need to change nome by value
+                    if (value != null && value.Length > 1)
+                    {
+                        _nome = value;
+                    }
                 }
             }
-        }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
+            public double Preco
+            {
+                get { return _preco; }
+            }
 
-        public int Quantidade
-        {
-            get { return _quantidade; }
-        }
+            public int Quantidade
+            {
+                get { return _quantidade; }
+            }
        
 
     
-        public double ValorTotalEmEstoque()
-        {
-            return _preco * _quantidade;
-        }
-        public void AdicionarProdutos(int quantidade)
-        {
-            _quantidade += quantidade;
-        }
-        public void RemoverProdutos(int quantidade)
-        {
-            _quantidade -= quantidade;
-        }
-        public override string ToString()
-        {
-            return Nome
-            + ", $ "
-            + Preco.ToString("F2")
-            + ", "
-            + Quantidade
-            + " unidades, Total: $ "
-            + ValorTotalEmEstoque().ToString("F2");
+            public double ValorTotalEmEstoque()
+            {
+                return _preco * _quantidade;
+            }
+            public void AdicionarProdutos(int quantidade)
+            {
+                _quantidade += quantidade;
+            }
+            public void RemoverProdutos(int quantidade)
+            {
+                _quantidade -= quantidade;
+            }
+            public override string ToString()
+            {
+                return Nome
+                + ", $ "
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2");
+            }
         }
     }
-}
