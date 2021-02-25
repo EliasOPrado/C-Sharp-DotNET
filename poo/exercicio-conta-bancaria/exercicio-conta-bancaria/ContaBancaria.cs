2982d419e53c40cd2c1993fed02f6112ab391539
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-24 21:12:08 -0300

    added conta bancaria

---
+++ poo/exercicio-conta-bancaria/exercicio-conta-bancaria/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace exercicio_conta_bancaria {
+    public class ContaBancaria {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public ContaBancaria(int numero, string titular){
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular) {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.00;
+        }
+
+        public override string ToString() {
+            return "Conta " +
+                Numero +
+                ", titular: " +
+                Titular +
+                ", saldo: $" +
+                Saldo.ToString("F2");
+        }
+
+    }
+}
---
+++ poo/exercicio-conta-bancaria/exercicio-conta-bancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace exercicio_conta_bancaria {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial (s/n): ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if(resp == 's' || resp == 'S') {
+                // saldo positivo
+                Console.Write("Entre o valor de deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            } else {
+                // saldo zero
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.Write("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito:");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque:");
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.WriteLine("Dados atualizados da conta");
+            Console.WriteLine(conta);
+        }
+    }
+}
