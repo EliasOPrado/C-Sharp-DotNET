44924c61d589f084b64fdfd5f56eaebbe1a31a46
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-25 08:26:21 -0300

    pasta matriz adicionado

---
+++ data-structures/matrizes/matrizes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            // estanciando matriz e o double tem 2 linhas e tres colunas
+            //double[,] mat = new double[2, 3];
+            
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for(int i = 0; i < n; i++) {
+                /*
+                 ira pegar tod-s os valores da primeira linha e separalos por espaco
+                 e esses valores serao adicionados em values string.
+                 */
+                string[] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < n; j++) {
+                    // o j sera colocado na frente do i na matriz.
+                    mat[i, j] = int.Parse(values[j]);
+                }
+
+            }
+
+            Console.WriteLine("Diagonal da matriz:");
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+
+            Console.WriteLine("Numeros negativos:");
+            int count = 0;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(mat[i,j] < 0) {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Numeros negativos: " + count);
+        }
+    }
+}
