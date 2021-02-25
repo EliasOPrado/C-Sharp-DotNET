7540405f9e83ea2073d6adab717dac1ba63c31c9
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-25 05:32:49 -0300

    adicionado modificador de parametro

---
+++ poo/modificador-de-parametro/modificador-de-parametro/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace modificador_de_parametro {
+    public class Calculator {
+
+        // Estes sao met-dos repetitivos
+
+        //public static int Sum(int n1, int n2) {
+        //    return n1 + n2;
+        //}
+        //public static int Sum(int n1, int n2, int n3) {
+        //    return n1 + n2 + n3;
+        //}
+        //public static int Sum(int n1, int n2, int n3, int n4) {
+        //    return n1 + n2 + n3 + n4;
+        //}
+
+        // solucao para nao repetir
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+
+            // pega o tamanho do vetor e adiciona ao numbers
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ poo/modificador-de-parametro/modificador-de-parametro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace modificador_de_parametro {
+    class Program {
+        static void Main(string[] args) {
+
+            // sem params
+            //int r1 = Calculator.Sum(new int[] { 10, 20, 30, 40 });// 100
+            //int r2 = Calculator.Sum(new int[] { 10, 20, 40 });// 70
+
+            // com params
+            int r1 = Calculator.Sum(10, 20, 30, 40 );// 100
+            int r2 = Calculator.Sum( 10, 20, 40 );// 70
+
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+
+        }
+    }
+}
