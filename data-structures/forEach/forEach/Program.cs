8a58aa07fccffb031492801642553c4c692de2e4
Elias Prado <eliaspradoprofessional@outlook.com>
2021-02-25 05:37:41 -0300

    adicionado exemplo foreach

---
+++ data-structures/forEach/forEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace forEach {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] vect = new string[] { "maria", "peter", "joao", "ana" };
+
+            for(int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("=========================");
+
+            // usando foreach
+            foreach(string obj in vect) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
